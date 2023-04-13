e3f9db7b945999ea889210afb03a5e30ae9a5756
Hex09AF <lsqhuy@gmail.com>
2023-04-14 02:58:57 +0700

    first-steps/load-models

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 import { World } from "./World/World.js";
 
 // create the main function
-function main() {
+async function main() {
   // Get a reference to the container element
   const container = document.querySelector("#scene-container");
 
-  // 1. Create an instance of the World app
+  // create a new world
   const world = new World(container);
+
+  // complete async tasks
+  await world.init();
 
   // 2. Render the scene
   world.start();
 }
 
 // call main to start the app
-main();
+main().catch((err) => {
+  console.error(err);
+});
